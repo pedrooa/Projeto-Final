ef53d27d1d713cc39169574ba31f834c13dbcf23
Vitor Satyro Vitturi <vitorsv1@al.insper.edu.br>
2017-05-02 20:32:39 -0300

    background inserido com window em tamanho igual

--- main.py
+++ main.py
@@ -1,12 +1,36 @@
 #Head Soccer Game - Vitor, Gabriel, Pedro e Manzanna
-import pyglet
+import pygame
 
-window = pyglet.window.Window()
-image = pyglet.resource.image('head.jpg')
+def main():
+    pygame.init()
+    bg = pygame.image.load('background2.jpeg')
+    tela = pygame.display.set_mode([803, 673])
+    pygame.display.set_caption('Head Soccer Game')
 
-@window.event
-def on_draw():
-    window.clear()
-    image.blit(0,0)
+    relogio = pygame.time.Clock()
+    cor_verde = (0,128,0)
 
-pyglet.app.run()
+    sair = True
+    while sair:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sair = False
+
+
+
+        #30 FPS
+        relogio.tick(25)
+        #Tela
+        tela.fill(cor_verde)
+        tela.blit(bg,(0,0,300,300))
+        #Atualiza a tela
+        pygame.display.update()
+
+
+
+#Fecha a janela e finaliza o jogo
+    pygame.quit()
+
+
+
+main()
