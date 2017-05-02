8563aba12f3fdb253711700e1a646d2e04d42814
Vitor Satyro Vitturi <vitorsv1@al.insper.edu.br>
2017-05-02 15:33:51 -0300

    jpg adicionado para teste

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 #Head Soccer Game - Vitor, Gabriel, Pedro e Manzanna
+import pyglet
+
+window = pyglet.window.Window()
+image = pyglet.resource.image('head.jpg')
+
+@window.event
+def on_draw():
+    window.clear()
+    image.blit(0,0)
+
+pyglet.app.run()
