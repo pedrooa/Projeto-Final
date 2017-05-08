49277f060880a0f2eb4512b80fcd0fdd071aaa5a
Ligeiro13 <gabriel.lrg@hotmail.com>
2017-05-08 00:07:54 -0300

    Add files via upload
    Pessoal aqui ta a minha versao do jogo com players(sprites sem imagem) com colisao apenas na lateral do mapa! Tbm, eu acho q o path ta funcionando direitinho. Deem um feedback!

---
+++ pygame_treino2.py
@@ -0,0 +1,111 @@
+# Pygame template - skeleton for a new pygame project
+import pygame
+import random
+from os import path
+
+WIDTH = 876
+HEIGHT = 573
+FPS = 60
+# define colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+
+
+class Player1(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50, 40))
+        self.image.fill(BLUE)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = WIDTH /10
+        self.rect.bottom = HEIGHT - 32
+        self.speedx = 0
+
+    def update(self):
+        self.speedx = 0
+        keystate = pygame.key.get_pressed()
+        if keystate[pygame.K_a]:
+            self.speedx = -8
+        if keystate[pygame.K_d]:
+            self.speedx = 8
+        self.rect.x += self.speedx
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+        if self.rect.left < 0:
+            self.rect.left = 0
+
+            
+class Player2(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50, 40))
+        self.image.fill(RED)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = WIDTH * 9/10
+        self.rect.bottom = HEIGHT - 32
+        self.speedx = 0
+
+    def update(self):
+        self.speedx = 0
+        keystate = pygame.key.get_pressed()
+        if keystate[pygame.K_LEFT]:
+            self.speedx = -8
+        if keystate[pygame.K_RIGHT]:
+            self.speedx = 8
+        self.rect.x += self.speedx
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+        if self.rect.left < 0:
+            self.rect.left = 0
+
+
+# initialize pygame and create window
+pygame.init()
+pygame.mixer.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Head Soccer pre-pre-pre-Alpha")
+clock = pygame.time.Clock()
+
+#carregando Imagens
+
+game_folder = path.dirname(__file__)
+img_folder = path.join(game_folder, "Imagens")
+#$bg = pygame.image.load('C:\\Users\\Gabrie Ligeiro\\Documents\\Insper\\software\\ep3\\Projeto-Final-master\\background2.jpeg')
+background = pygame.image.load(path.join(img_folder, 'background2.jpeg')).convert()
+background_rect = background.get_rect()
+#Sprites
+all_sprites = pygame.sprite.Group()
+player1 = Player1()
+player2 = Player2()
+all_sprites.add(player1) #ADD Sprites
+all_sprites.add(player2)
+# Game loop
+running = True
+while running:
+    # keep loop running at the right speed
+    clock.tick(FPS)
+    # Process input (events)
+    for event in pygame.event.get():
+        # check for closing window
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Update
+    all_sprites.update()
+    #checando se um jogador colidiou com op outro
+    #colisao = pygame.sprite.spritecollide(player1, player2, False) #devolve uma lista!
+    #print(colisao
+
+    # Draw / render
+    screen.fill(BLACK)
+    screen.blit(background, background_rect)
+    all_sprites.draw(screen) #rodando os sprites
+    pygame.draw.rect(screen, GREEN, [0,HEIGHT - 30,WIDTH,30])
+    # *after* drawing everything, flip the display
+    pygame.display.flip()
+
+pygame.quit()
