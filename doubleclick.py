8eeb4cf06091ea23026dea4f9fd5b76acc8ecaf9
Vitor Satyro Vitturi <vitorsv1@al.insper.edu.br>
2017-05-12 17:43:40 -0300

    adicionei imagem do mato e mudei um pouco doubleclick(ainda não finalizado)

--- doubleclick.py
+++ doubleclick.py
@@ -12,49 +12,23 @@ seta_direita = 275
 seta_esquerda = 276
 
 clock = pygame.time.Clock()
-timer = 0
-timer2 = 0
 dt = 0.1
 
 while exit:
 	for event in pygame.event.get():
+		timer = 0
 		if event.type == pygame.QUIT:
 			exit = False
 		if event.type == pygame.KEYDOWN:
-			if event.key == seta_direita:		
-				print("Moveu Direita")
-				if timer == 0:  # First click.
-				 	timer = 0.001 # Start the timer.
-				 	print("s")
-				if timer != 0 :
-					print("k")
+				if event.key == seta_direita:		
+					print("Moveu Direita")
+					timer = 0.001
+		while timer < 0.2:
+			if event.type == pygame.KEYDOWN:
+				if event.key == seta_direita:		
+					print("dash")
 					timer += dt
-					pygame.time.delay(100)
-				#Click again before 0.3 seconds to double click.
-				elif timer < 0.3:
-					print('Dash Direita')
-					timer = 0					
-				# Increase timer after mouse was pressed the first time.
-				
-				# Reset after 0.3 seconds.
-				elif timer >= 0.3:
-					timer = 0
-			elif event.key == seta_esquerda:
-				print("Moveu Esquerda")
-				if timer2 == 0:  # First click.
-				 	timer2 = 0.001 # Start the timer.
-				#Click again before 0.3 seconds to double click.
-				if timer2 < 0.3:
-					print('Dash Esquerda')
-					timer2 = 0
-				# Increase timer after mouse was pressed the first time.
-				if timer2 != 0:
-					timer2 += dt
-					pygame.time.delay(100)
-				# Reset after 0.3 seconds.
-				if timer2 >= 0.3:
-					timer2 = 0
-		
-pygame.display.update()
+
+	pygame.display.update()
 pygame.quit()
 quit()
