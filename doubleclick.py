a85a5841fd9aca4bfd292e32cf884b34a1a2aaff
Vitor Satyro Vitturi <vitorsv1@al.insper.edu.br>
2017-05-16 14:03:37 -0300

    testando doubleclick

--- doubleclick.py
+++ doubleclick.py
@@ -11,35 +11,22 @@ seta_baixo = 274
 seta_direita = 275
 seta_esquerda = 276
 
-global clock, double_click_event, timer
-double_click_event = pygame.USEREVENT + 1
+clock = pygame.time.Clock()
 timer = 0
-
+dt = 0
 
 while exit:
 	
+
 	for event in pygame.event.get():
 		timer = 0
 		if event.type == pygame.QUIT:
 			exit = False
-		if event.type == pygame.K_RIGHT:
-			if timer == 0:
-				pygame.time.set_timer(double_click_event, 500)
-				timerset = True
-			else:
-				if timer == 1:
-					pygame.time.set_timer(double_click_event, 0)
-					double_click()
-					timerset = False
-			if timerset:
-				timer = 1
-			else: 
-				timer = 0
-		elif event.type == double_click_event:
-			# timer timed out
-			pygame.time.set_timer(double_click_event, 0)
-			timer = 0
-			print("evt = dble click")
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_p:
+				print(pygame.key.set_repeat(1,200))
+
+
 
 	pygame.display.update()
 pygame.quit()
